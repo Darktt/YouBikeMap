ab895a06d98e220df172b3f999924855b6d851fa
Eden <pkh_eden@photons.com.tw>
2023-07-04 10:05:39 +0800

    Initial Commit

---
+++ YouBikeMap/YouBikeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouBikeMapApp.swift
+//  YouBikeMap
+//
+//  Created by Eden on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct YouBikeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
