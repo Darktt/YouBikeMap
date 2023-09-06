f55b0423fd7452b558e2e0aaae794afbba1732ba
Eden <pkh_eden@photons.com.tw>
2023-09-06 15:54:05 +0800

    新增 json 欄位描述

--- YouBikeMap/Models/YouBikeMapItem.swift
+++ YouBikeMap/Models/YouBikeMapItem.swift
@@ -9,6 +9,27 @@ import Foundation
 import CoreLocation
 import JsonProtection
 
+/**
+     sno(站點代號)、
+     sna(場站中文名稱)、
+     tot(場站總停車格)、
+     sbi(場站目前車輛數量)、
+     sarea(場站區域)、
+     mday(資料更新時間)、
+     lat(緯度)、
+     lng(經度)、
+     ar(地點)、
+     sareaen(場站區域英文)、
+     snaen(場站名稱英文)、
+     aren(地址英文)、
+     bemp(空位數量)、
+     act(全站禁用狀態)、
+     srcUpdateTime(YouBike2.0系統發布資料更新的時間)、
+     updateTime(大數據平台經過處理後將資料存入DB的時間)、
+     infoTime(各場站來源資料更新時間)、
+     infoDate(各場站來源資料更新時間)
+ */
+
 public
 struct YouBikeMapItem
 {
